ea6f299dac968658362eb377d572621f9af7fa24
surani-hub <skmali1357@gmail.com>
2022-11-14 19:55:58 +0530

    solutions

--- assignments/02-first-java/code/src/main/java/Greeting.java
+++ assignments/02-first-java/code/src/main/java/Greeting.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 */
 public class Greeting {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
         System.out.println("Input name: ");
         String name = in.nextLine();
