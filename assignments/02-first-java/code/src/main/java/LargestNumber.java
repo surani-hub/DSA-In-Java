34de9c29fb9155a7676620e7dcffb78600c652cc
surani-hub <skmali1357@gmail.com>
2022-11-14 19:56:15 +0530

    solutions

--- assignments/02-first-java/code/src/main/java/LargestNumber.java
+++ assignments/02-first-java/code/src/main/java/LargestNumber.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 */
 public class LargestNumber {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
         System.out.println("Input two numbers ");
         int num1 = in.nextInt();
