8c7d066418166a645fb8ca83e3d4edf74064b05d
surani-hub <skmali1357@gmail.com>
2022-11-14 19:56:30 +0530

    solutions

--- assignments/02-first-java/code/src/main/java/Palindrome.java
+++ assignments/02-first-java/code/src/main/java/Palindrome.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 */
 public class Palindrome {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
 
