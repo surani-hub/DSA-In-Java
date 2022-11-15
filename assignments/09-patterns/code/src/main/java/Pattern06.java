b34896bfdca071a0bfe8c1fc88de6c7c6fc5c5c0
surani-hub <skmali1357@gmail.com>
2022-11-15 21:13:59 +0530

    pattern solutions

---
+++ assignments/09-patterns/code/src/main/java/Pattern06.java
@@ -0,0 +1,31 @@
+/*
+	Write a program to print following pattern?
+	 	
+	    *
+	   **
+	  ***
+	 ****
+	*****
+*/
+
+
+import java.util.Scanner;
+
+public class Pattern06{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for(int i=1;i<=num;i++){
+            for(int j=num-1;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
