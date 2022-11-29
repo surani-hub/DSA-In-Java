35ebebdd57aef694942b35b7d6cd3f6af5f6b596
surani-hub <skmali1357@gmail.com>
2022-11-29 22:33:31 +0530

    internal implementation of linked list

--- assignments/15-linked-list/code/src/main/java/LLTest.java
+++ assignments/15-linked-list/code/src/main/java/LLTest.java
@@ -5,11 +5,11 @@ class LLTest{
         //list.addFirst(10);
 
         list.addFirst(20);
-		/*
-		list.addFirst(30);
-		list.addFirst(40);
-		list.addFirst(50);
-		*/
+
+        list.addFirst(30);
+        list.addFirst(40);
+        list.addFirst(50);
+
         list.display();
 		/*
 		list.addLast(110);
@@ -21,7 +21,9 @@ class LLTest{
 		list.display();
 		*/
         System.out.println(list.size());
-        System.out.println(list.deleteLast());
+        //System.out.println(list.delete(2));
+        list.find(40);
         System.out.println(list.size());
+
     }
 }
