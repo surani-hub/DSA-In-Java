1b584f4fd93e497ef95be04eb1931bf8fd72e6fd
surani-hub <skmali1357@gmail.com>
2022-12-05 17:19:27 +0530

    cyclic sort pattern

---
+++ assignments/07-sorting/code/src/main/java/FindAllDuplicates.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/* https://leetcode.com/problems/find-all-duplicates-in-an-array/description/*/
+public class FindAllDuplicates {
+    public static void main(String[] args) {
+        int[] nums = {4,3,2,7,8,2,3,1};
+        System.out.println(findDuplicates(nums));
+    }
+
+    static public List<Integer> findDuplicates(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index + 1) {
+                list.add(arr[index]);
+                ;
+            }
+        }
+        return list;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
