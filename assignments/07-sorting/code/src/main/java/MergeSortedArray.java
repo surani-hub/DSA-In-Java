83a608cf1b56137d7b537709e81093044a80bf22
surani-hub <skmali1357@gmail.com>
2022-12-07 23:09:21 +0530

    merged sorted arrays

---
+++ assignments/07-sorting/code/src/main/java/MergeSortedArray.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+/* https://leetcode.com/problems/merge-sorted-array/description/ */
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 ={1,2,3};
+        int[] nums2 ={2,5,6};
+        int m = 3, n = 3;
+        merge(nums1, m, nums2, n);
+    }
+
+    static public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] nums3 = new int[m + n];
+        for (int i = 0; i < m; i++) {
+            nums3[i] = nums1[i];
+        }
+        int z = 0;
+        for (int k = m; k < nums3.length; k++) {
+            nums3[k] = nums2[z++];
+        }
+        Arrays.sort(nums3);
+        System.out.println(Arrays.toString(nums3));
+    }
+}
