a8306da80cfb3be2fa69103bd761264748ce7ac1
surani-hub <skmali1357@gmail.com>
2022-12-09 23:13:15 +0530

    lambda expression for functional interfaces

---
+++ assignments/16-lambda-expression/code/src/com/sudha/lambda/Main.java
@@ -0,0 +1,8 @@
+package com.sudha.lambda;
+
+public class Main {
+    public static void main(String[] args) {
+        Interf i = ()->System.out.println("interface method definition");
+        i.m1();
+    }
+}
