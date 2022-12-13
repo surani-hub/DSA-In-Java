9c1050ba12762a377840eba0de9cf302614649ce
surani-hub <skmali1357@gmail.com>
2022-12-13 22:10:54 +0530

    in-place reversal of the string

---
+++ assignments/07-sorting/code/src/main/java/ReverseString.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        char[] s ={'h', 'a', 'n', 'n', 'a', 'h'}; // 'h', 'e', 'l', 'l', 'o'
+        reverseString(s);
+    }
+
+    static public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+        while (start <= end) {
+            swap(s, start, end);
+            start++;
+            end--;
+        }
+        System.out.println(Arrays.toString(s));
+    }
+
+    static public void swap(char[] s, int first, int second) {
+        char temp = s[first];
+        s[first] = s[second];
+        s[second] = temp;
+    }
+}
