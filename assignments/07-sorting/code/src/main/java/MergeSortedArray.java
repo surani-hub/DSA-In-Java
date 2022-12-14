2bf16801832ecc594e325d969cabc570007623e3
surani-hub <skmali1357@gmail.com>
2022-12-14 22:09:20 +0530

    merge sorted array

--- assignments/07-sorting/code/src/main/java/MergeSortedArray.java
+++ assignments/07-sorting/code/src/main/java/MergeSortedArray.java
@@ -3,10 +3,12 @@ import java.util.Arrays;
 /* https://leetcode.com/problems/merge-sorted-array/description/ */
 public class MergeSortedArray {
     public static void main(String[] args) {
-        int[] nums1 ={1,2,3};
-        int[] nums2 ={2,5,6};
+        int[] nums1 = {1, 2, 3};
+        int[] nums2 = {2, 5, 6};
         int m = 3, n = 3;
         merge(nums1, m, nums2, n);
+
+        mergeSorted(nums1, m, nums2, n);
     }
 
     static public void merge(int[] nums1, int m, int[] nums2, int n) {
@@ -20,5 +22,23 @@ public class MergeSortedArray {
         }
         Arrays.sort(nums3);
         System.out.println(Arrays.toString(nums3));
+    }
+
+    static public void mergeSorted(int[] nums1, int m, int[] nums2, int n) {
+        int k = m + n - 1;
+        int p1 = m - 1;
+        int p2 = n - 1;
+
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[k--] = nums1[p1--];
+            } else {
+                nums1[k--] = nums2[p2--];
+            }
+        }
+        while (p2 >= 0) {
+            nums1[k--] = nums2[p2--];
+        }
+        System.out.println(Arrays.toString(nums1));
     }
 }
