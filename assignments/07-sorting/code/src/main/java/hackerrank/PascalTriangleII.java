92c5f45844d1982845ed1b04d1819c91c51fc72b
surani-hub <skmali1357@gmail.com>
2023-02-17 14:03:41 +0530

    pascal triangle specific row

---
+++ assignments/07-sorting/code/src/main/java/hackerrank/PascalTriangleII.java
@@ -0,0 +1,22 @@
+package hackerrank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangleII {
+    public static void main(String[] args) {
+        int rowIndex = 3;
+        System.out.println(getRow(rowIndex));
+    }
+
+    static public List<Integer> getRow(int rowIndex) {
+        List<Integer> ans = new ArrayList<Integer>();
+        for (int i = 0; i < rowIndex + 1; i++) {
+            ans.add(1);
+            for (int j = i - 1; j > 0; j--) {
+                ans.set(j, ans.get(j - 1) + ans.get(j));
+            }
+        }
+        return ans;
+    }
+}
