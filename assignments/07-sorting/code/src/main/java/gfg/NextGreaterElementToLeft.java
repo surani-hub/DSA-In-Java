49fd803855d33f4d6d959d02e92a29699d1e9b64
surani-hub <skmali1357@gmail.com>
2023-02-24 22:38:24 +0530

    next greater element to the left using stack

---
+++ assignments/07-sorting/code/src/main/java/gfg/NextGreaterElementToLeft.java
@@ -0,0 +1,29 @@
+package gfg;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class NextGreaterElementToLeft {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 2, 4};
+        System.out.println(Arrays.toString(findNextGreaterOptimised(arr)));
+    }
+
+    // time complexity is O(n)
+    static public int[] findNextGreaterOptimised(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                ans[i] = -1;
+            } else {
+                ans[i] = stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        return ans;
+    }
+}
