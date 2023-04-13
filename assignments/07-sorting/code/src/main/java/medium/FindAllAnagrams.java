50d8daa459b2fd6320bd546b16bd8e889e9a24e4
surani-hub <skmali1357@gmail.com>
2023-04-13 22:43:55 +0530

    anagrams

--- assignments/07-sorting/code/src/main/java/medium/FindAllAnagrams.java
+++ assignments/07-sorting/code/src/main/java/medium/FindAllAnagrams.java
@@ -6,11 +6,50 @@ import java.util.List;
 
 public class FindAllAnagrams {
     public static void main(String[] args) {
-    //    String s = "dinitrophenylhydrazinetrinitrophenylmethylnitramine", p = "trinitrophenylmethylnitramine";
-        String s= "cbaebabacd", p ="abc";
+//        String s = "dinitrophenylhydrazinetrinitrophenylmethylnitramine", p = "trinitrophenylmethylnitramine"; // [19, 20, 21, 22, 23]
+        String s = "cbaebabacd", p = "abc"; // [0, 6]
         System.out.println(findAnagrams(s, p));
     }
 
+    //    static public List<Integer> findAnagrams(String s, String p) {
+//        int len1 = s.length(), len2 = p.length();
+//        List<Integer> ans = new ArrayList<>();
+//        if (len1 < len2) {
+//            return ans;
+//        }
+//        HashMap<Character, Integer> map = new HashMap<>();
+//        for (int i = 0; i < len2; i++) {
+//            map.put(p.charAt(i), map.getOrDefault(p.charAt(i), 0) + 1);
+//        }
+//        int windowSize = len2;
+//        int left = 0, right = 0;
+//        while (right < len1) {
+//            char c = s.charAt(right);
+//            if (map.containsKey(c)) {
+//                int frequency = map.get(c);
+//                map.put(c, frequency - 1);
+//                if (frequency > 0) {
+//                    windowSize--;
+//                }
+//            }
+//            right++;
+//            if (right - left == len2) {
+//                char c1 = s.charAt(left);
+//                if (map.containsKey(c1)) {
+//                    int freq = map.get(c1);
+//                    map.put(c1, freq + 1);
+//                    if (freq == 0 ) {
+//                        windowSize++;
+//                    }
+//                }
+//                left++;
+//            }
+//            if (windowSize == 0) {
+//                ans.add(left);
+//            }
+//        }
+//        return ans;
+//    }
     static public List<Integer> findAnagrams(String s, String p) {
         int len1 = s.length(), len2 = p.length();
         List<Integer> ans = new ArrayList<>();
@@ -33,12 +72,12 @@ public class FindAllAnagrams {
                 }
             }
             right++;
-            if ((right - left) == len2) {
+            if (right - left == len2) {
                 char c1 = s.charAt(left);
                 if (map.containsKey(c1)) {
                     int freq = map.get(c1);
                     map.put(c1, freq + 1);
-                    if (freq == 0 ) {
+                    if (freq == 0) {
                         windowSize++;
                     }
                 }
