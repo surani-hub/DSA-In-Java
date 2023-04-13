ed7f143c0e78cdd4a2a4c89dc4de1a27612a1508
surani-hub <skmali1357@gmail.com>
2023-04-13 22:42:55 +0530

    power of 3

--- assignments/07-sorting/code/src/main/java/medium/CheckPowerOf3.java
+++ assignments/07-sorting/code/src/main/java/medium/CheckPowerOf3.java
@@ -4,7 +4,21 @@ package medium;
 public class CheckPowerOf3 {
     public static void main(String[] args) {
         int n = 91;
-        System.out.println(checkPowersOfThree(n));
+        // System.out.println(checkPowersOfThree(n));
+        int num = 13;
+        System.out.println(countDigitOne(num));
+    }
+
+    static public int countDigitOne(int n) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+                int rem = i / 10;
+                if (rem == 1) {
+                    count++;
+                }
+                i = i / 10;
+            }
+        return count;
     }
 
     static public boolean checkPowersOfThree(int n) {
